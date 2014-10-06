e51f0e7f1e0c68b1a07e57eb575fa9105ddddeab
malex <malex@calclab.com>
2014-10-06 22:23:07 +0200

    Improve middleware for accessing old website data
    Also fixing formatting (tabs/spaces issue).

--- lib/calcaxy_old.rb
+++ lib/calcaxy_old.rb
@@ -2,46 +2,37 @@
 
 class CalcaxyOld
 
-	DEFAULT_FILES = ["index.html", "index.htm", "index.php"]
+  DEFAULT_FILES = ["index.html", "index.htm", "index.php"]
   File = ::File
 
   def initialize(app, archive_path)
     @app = app
-		@archive_path = archive_path
+    @archive_path = archive_path
   end
 
   def call(env)
+    req = Rack::Request.new(env)
     status, header, response = @app.call(env)
-		path = env['PATH_INFO']
-    if status == 404
-			if  path.start_with? @archive_path
-				path = path[@archive_path.length .. -1]
-	      absolute_file = absolute_rails_path path
-	      # extend with first found default file name if a directory
-				Rails.logger.info "*** Checking #{path}... Directory? #{File.directory?(absolute_file)}"
-				default_file = nil
-	      if File.directory?(absolute_file)
-	      	default_file = DEFAULT_FILES.first { |f| File.file?(File.join(@absolute_path,f)) }
-					Rails.logger.info "*** Found default file #{default_file}"
-					# extend requested path with a default file name
-					path = File.join(path, default_file) unless default_file.nil?
-	      end
-				Rails.logger.info "*** New file: #{path}!"
+    resource_path = req.path
 
-	      if File.exists?(absolute_rails_path(path))
-	        return [301, {"Location" => path, "Content-Type" => "text/html"}, "Redirecting to: #{path}"]
-	#        content = File.read(file)
-	#        length = "".respond_to?(:bytesize) ? content.bytesize.to_s : content.size.to_s
-	#        [503, {'Content-Type' => 'text/html', 'Content-Length' => length}, [content]]
-				end
-			end
+    if status == 404
+      if  req.path.start_with? @archive_path
+        absolute_resource_path = absolute_rails_path resource_path
+
+        if File.directory?(absolute_resource_path)
+          default_file = DEFAULT_FILES.first { |f| File.file?(File.join(absolute_resource_path,f)) }
+          # extend requested path with a default file name
+          resource_path = File.join(resource_path, default_file) unless default_file.nil?
+        end
+        return [301, {"Location" => resource_path, "Content-Type" => "text/html"}, "Redirecting to: #{resource_path}"]
+      end
     end
-		[status, header, response]
+    [status, header, response]
   end
 
-	private
+  private
 
-	  def absolute_rails_path(path)
-			File.join(Rails.root, "public", path)
-		end
+    def absolute_rails_path(path)
+      File.join(Rails.root, "public", path)
+    end
 end
